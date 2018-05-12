728837170e5fd364bd91c222196a266e31b84eed
coolchicprogramer <coolchicprogramer@gmail.com>
2018-05-12 23:00:14 +0900

    Initial Commit

---
+++ cups-sample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  cups-sample
+//
+//  Created by user on 2018/05/12.
+//  Copyright © 2018年 coolchic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
